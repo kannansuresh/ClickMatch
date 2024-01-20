7cc1f66bec538ee61329669748c3a26c3d0874f2
Kannan Suresh <kannansuresh@outlook.com>
2024-01-20 09:21:28 +0530

    Added update available component and other associated code.

--- src/Aneejian.Games.ClickMatch.Client/wwwroot/service-worker.published.js
+++ src/Aneejian.Games.ClickMatch.Client/wwwroot/service-worker.published.js
@@ -19,6 +19,8 @@ const manifestUrlList = self.assetsManifest.assets.map(asset => new URL(asset.ur
 async function onInstall(event) {
     console.info('Service worker: Install');
 
+    self.skipWaiting();
+
     // Fetch and cache all matching items from the assets manifest
     const assetsRequests = self.assetsManifest.assets
         .filter(asset => offlineAssetsInclude.some(pattern => pattern.test(asset.url)))
