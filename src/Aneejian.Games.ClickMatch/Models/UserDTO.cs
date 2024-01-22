f10427fc3be377473a65b4e1ecc747da388d40b7
Kannan Suresh <kannansuresh@outlook.com>
2024-01-22 09:42:59 +0530

    Added some base classes and dexie to start with IndexedDB implementation.

---
+++ src/Aneejian.Games.ClickMatch/Models/UserDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Aneejian.Games.ClickMatch.Models;
+
+public class UserDTO
+{
+	[Key]
+	public int Id { get; set; }
+
+	[Required]
+	public string Name { get; set; } = string.Empty;
+
+	[Required]
+	public  string UserName { get; set; } = string.Empty;
+
+	[Required]
+	public string Password { get; set; } = string.Empty;
+}
