34b82e46277578bd33efeaa8b7b7491999bc358a
Kannan Suresh <kannansuresh@outlook.com>
2024-02-09 21:30:39 +0530

    Multiple upates.

--- src/Aneejian.Games.ClickMatch/Constants/AppStrings.cs
+++ src/Aneejian.Games.ClickMatch/Constants/AppStrings.cs
@@ -3,6 +3,7 @@
 public struct AppStrings
 {
 	public const string ConfigFilePath = "config/config.json?v=1.0.0";
+	public const string GithubRepo = "https://github.com/kannansuresh/ClickMatch";
 
 	public struct Pages
 	{
@@ -10,6 +11,7 @@ public struct AppStrings
 		public const string Game = "game";
 		public const string Login = "login";
 		public const string Register = "register";
+		public const string About = "about";
 	}
 
 	public struct SessionStorageKeys
---
+++ src/Aneejian.Games.ClickMatch/Helpers/SharedMethods.cs
@@ -0,0 +1,24 @@
+﻿using Aneejian.Games.ClickMatch.Constants;
+using Aneejian.Games.ClickMatch.Data;
+
+namespace Aneejian.Games.ClickMatch.Helpers;
+
+public static class SharedMethods
+{
+	public static string WebpProfileImage(UserDto profile)
+	{
+		if (profile.Avatar is null)
+		{
+			return "";
+		}
+		if (profile.Avatar == AppStrings.GuestUser.Avatar)
+		{
+			return Path.ChangeExtension(profile.Avatar, "webp");
+		}
+		if (profile.Avatar.Contains("aneejian.com", StringComparison.CurrentCultureIgnoreCase) || profile.Avatar.Contains("kannansuresh", StringComparison.CurrentCultureIgnoreCase))
+		{
+			return profile.Avatar.ToWebp();
+		}
+		return profile.Avatar;
+	}
+}
