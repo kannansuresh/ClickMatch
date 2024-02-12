9fe1e852bdbc8eb39c89d7f573273caf7b35a047
Kannan Suresh <kannansuresh@outlook.com>
2024-02-12 06:22:22 +0530

    Update custom.js file

--- src/Aneejian.Games.ClickMatch.Client/wwwroot/js/custom.js
+++ src/Aneejian.Games.ClickMatch.Client/wwwroot/js/custom.js
@@ -98,3 +98,7 @@ function setModalFocus(myModalEl) {
     if (inputElement)
         inputElement.focus()
 }
+
+function scrollElementIntoView(elementId) {
+    document.getElementById(elementId).scrollIntoView({ behavior: "smooth", block: "center", inline: "nearest" });
+}
