3922bafb07e0f51de98ce9ac7158ccd762a21fbf
Kannan Suresh <kannansuresh@outlook.com>
2024-02-20 21:55:31 +0530

    - Added biome via npm - Added biome.json - Added words to cspell.json - Updated workspace settings

--- src/Aneejian.Games.ClickMatch/wwwroot/js/UserDTO.js
+++ src/Aneejian.Games.ClickMatch/wwwroot/js/UserDTO.js
@@ -1,12 +1,9 @@
-
-
-
 export class UserDTO {
-    constructor(user) {
-        this.id = user.id === 0 || null ? undefined : user.id;
-        this.userName = user.userName;
-        this.name = user.name;
-        this.password = user.password;
-        this.avatar = user.avatar;
-    }
+	constructor(user) {
+		this.id = user.id === 0 || null ? undefined : user.id;
+		this.userName = user.userName;
+		this.name = user.name;
+		this.password = user.password;
+		this.avatar = user.avatar;
+	}
 }
